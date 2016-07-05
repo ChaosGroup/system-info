3eff1865c90a59c3c13bb6d0f1bf50df265a5f27
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2016-07-05 18:15:46 +0300

    Get the CPUs' names (on Linux and Windows)

--- src/System/Information.hs
+++ src/System/Information.hs
@@ -1,10 +1,30 @@
 {-# Language CPP #-}
 module System.Information
   ( OS(..), os
+  , CPUName, CPUNames, cpuNames
   ) where
+
+#ifdef linux_HOST_OS
+import Data.List (isPrefixOf)
+
+#elif mingw32_HOST_OS
+import Control.Monad (forM)
+import System.Win32.Registry
+  ( hKEY_LOCAL_MACHINE
+  , regOpenKey, regCloseKey, regQueryInfoKey, regQueryValue
+  , subkeys)
+
+#endif
+
 
 data OS = Linux | Windows | Other
   deriving (Eq, Show)
+
+newtype CPUName = CPUName String
+  deriving (Eq, Ord, Show)
+
+type CPUNames = [CPUName]
+
 
 os :: OS
 os =
@@ -14,4 +34,37 @@ os =
   Windows
 #else
   Other
+#endif
+
+cpuNames :: IO CPUNames
+cpuNames =
+#ifdef linux_HOST_OS
+  linuxCPUNames
+#elif mingw32_HOST_OS
+  windowsCPUNames
+#else
+  pure []
+#endif
+
+#ifdef linux_HOST_OS
+linuxCPUNames :: IO CPUNames
+linuxCPUNames = do
+  lines' <- lines <$> readFile "/proc/cpuinfo"
+  pure . map (CPUName . unwords . drop 3 . words) $
+    filter ("model name" `isPrefixOf`) lines'
+
+#elif mingw32_HOST_OS
+windowsCPUNames :: IO CPUNames
+windowsCPUNames = do
+  cpus <- regOpenKey hKEY_LOCAL_MACHINE "Hardware\\Description\\System\\CentralProcessor"
+  n <- subkeys <$> regQueryInfoKey cpus
+  res <- forM [0..n-1] $ \i -> do
+    cpu <- regOpenKey cpus $ show i
+    cpuName <- regQueryValue cpu $ Just "ProcessorNameString"
+    regCloseKey cpu
+    pure cpuName
+
+  regCloseKey cpus
+  pure $ map (CPUName . unwords . words) res
+
 #endif
