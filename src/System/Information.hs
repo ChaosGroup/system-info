ae570427c21e22c26abdaf4b4891db8da647831e
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2016-07-05 18:03:42 +0300

    Get the operating system's name (for Windows and Linux)

--- src/System/Information.hs
+++ src/System/Information.hs
@@ -1,1 +1,17 @@
-module System.Information where
+{-# Language CPP #-}
+module System.Information
+  ( OS(..), os
+  ) where
+
+data OS = Linux | Windows | Other
+  deriving (Eq, Show)
+
+os :: OS
+os =
+#ifdef linux_HOST_OS
+  Linux
+#elif mingw32_HOST_OS
+  Windows
+#else
+  Other
+#endif
