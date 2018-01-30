8f4084cb520be4186004c4d34f12d41d634f1d8d
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-01-30 19:09:06 +0200

    Return HRESULTs and use out-parameters for the pointers

--- csrc/wmi.c
+++ csrc/wmi.c
@@ -3,192 +3,138 @@
 #include <stdbool.h>
 
 
-bool is_valid_hresult(HRESULT* hres) {
-  if (hres == NULL) {
-    hres = new HRESULT;
-    *hres = E_POINTER;
+HRESULT init() {
+  HRESULT hres = CoInitializeEx(0, COINIT_MULTITHREADED);
 
-    return false;
+  if (!FAILED(hres)) {
+    hres = CoInitializeSecurity(
+      NULL,
+      -1,
+      NULL,
+      NULL,
+      RPC_C_AUTHN_LEVEL_DEFAULT,
+      RPC_C_IMP_LEVEL_IMPERSONATE,
+      NULL,
+      EOAC_NONE,
+      NULL);
   }
-  return true;
+  return hres;
 }
 
-void init(HRESULT* hres) {
-  if (is_valid_hresult(hres)) {
-    *hres = CoInitializeEx(0, COINIT_MULTITHREADED);
-  }
-
-  if (FAILED(*hres)) return;
-
-  *hres = CoInitializeSecurity(
-    NULL,
-    -1,
-    NULL,
-    NULL,
-    RPC_C_AUTHN_LEVEL_DEFAULT,
-    RPC_C_IMP_LEVEL_IMPERSONATE,
-    NULL,
-    EOAC_NONE,
-    NULL);
+HRESULT createInstance(IWbemLocator* plocator) {
+  return CoCreateInstance(
+    &CLSID_WbemLocator,
+    0,
+    CLSCTX_INPROC_SERVER,
+    &IID_IWbemLocator, (LPVOID *)&plocator);
 }
 
-IWbemLocator* createInstance(HRESULT* hres) {
-  IWbemLocator *plocator = NULL;
+HRESULT connectServer(IWbemLocator* plocator, IWbemServices* pservices) {
+  if (!plocator) return E_INVALIDARG;
 
-  if (is_valid_hresult(hres)) {
-    *hres = CoCreateInstance(
-      &CLSID_WbemLocator,
-      0,
-      CLSCTX_INPROC_SERVER,
-      &IID_IWbemLocator, (LPVOID *)&plocator);
-  }
+  HRESULT hres = plocator->lpVtbl->ConnectServer(
+    plocator,
+    SysAllocString(L"ROOT\\CIMV2"), // Object path of WMI namespace
+    NULL,                           // User name. NULL = current user
+    NULL,                           // User password. NULL = current
+    NULL,                           // Locale. NULL indicates current
+    0,                              // Security flags.
+    NULL,                           // Authority (for example, Kerberos)
+    NULL,                           // Context object
+    &pservices);
 
-  if (FAILED(*hres)) {
-    plocator = NULL;
-  }
-  return plocator;
-}
+  if (!FAILED(hres)) {
+    hres = CoSetProxyBlanket(
+      (IUnknown *)pservices,
+      RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+      RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+      NULL,                        // Server principal name
+      RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx
+      RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+      NULL,                        // client identity
+      EOAC_NONE);                  // proxy capabilities
 
-IWbemServices* connectServer(HRESULT* hres, IWbemLocator *plocator) {
-  IWbemServices *pservices = NULL;
-
-  if (is_valid_hresult(hres)) {
-    if (plocator != NULL) {
-      *hres = plocator->lpVtbl->ConnectServer(
-        plocator,
-        SysAllocString(L"ROOT\\CIMV2"), // Object path of WMI namespace
-        NULL,                           // User name. NULL = current user
-        NULL,                           // User password. NULL = current
-        NULL,                           // Locale. NULL indicates current
-        0,                              // Security flags.
-        NULL,                           // Authority (for example, Kerberos)
-        NULL,                           // Context object
-        &pservices);
-    } else {
-      *hres = E_POINTER;
+    if (FAILED(hres)) {
+      pservices->lpVtbl->Release(pservices);
+      pservices = NULL;
     }
   }
 
-  if (FAILED(*hres)) {
-    return NULL;
-  }
-
-  *hres = CoSetProxyBlanket(
-    (IUnknown *)pservices,
-    RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
-    RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
-    NULL,                        // Server principal name
-    RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx
-    RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-    NULL,                        // client identity
-    EOAC_NONE);                  // proxy capabilities
-
-  if (FAILED(*hres)) {
-    pservices->lpVtbl->Release(pservices);
-    pservices = NULL;
-  }
-
-  return pservices;
+  return hres;
 }
 
-IEnumWbemClassObject* query(HRESULT* hres, IWbemServices* pservices, BSTR query) {
-  IEnumWbemClassObject* penumerator = NULL;
+HRESULT query(IWbemServices* pservices, BSTR query, IEnumWbemClassObject* penumerator) {
+  if (!pservices) return E_INVALIDARG;
 
-  if (is_valid_hresult(hres)) {
-    if (pservices != NULL) {
-      *hres = pservices->lpVtbl->ExecQuery(
-        pservices,
-        SysAllocString(L"WQL"),
-        query,
-        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-        NULL,
-        &penumerator);
-    } else {
-      *hres = E_POINTER;
-    }
-  }
-
-  if (FAILED(*hres)) {
-    penumerator = NULL;
-  }
-
-  return penumerator;
+  return pservices->lpVtbl->ExecQuery(
+    pservices,
+    SysAllocString(L"WQL"),
+    query,
+    WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+    NULL,
+    &penumerator);
 }
 
-wchar_t* getStringField(HRESULT* hres, IEnumWbemClassObject* penumerator, BSTR field) {
-  wchar_t* res = L"";
+HRESULT getStringField(IEnumWbemClassObject* penumerator, BSTR field, wchar_t* value) {
+  if (!penumerator) return E_INVALIDARG;
+
   IWbemClassObject *pclassObject = NULL;
   ULONG ures = 0;
+  HRESULT hres = penumerator->lpVtbl->Next(penumerator, WBEM_INFINITE, 1, &pclassObject, &ures);
 
-  if (is_valid_hresult(hres)) {
-    if (penumerator != NULL) {
-      *hres = penumerator->lpVtbl->Next(penumerator, WBEM_INFINITE, 1, &pclassObject, &ures);
-    } else {
-      *hres = E_POINTER;
+  if (!FAILED(hres) && ures) {
+    VARIANT vProperty;
+    hres = pclassObject->lpVtbl->Get(pclassObject, field, 0, &vProperty, 0, 0);
+
+    if (!FAILED(hres)) {
+      value = vProperty.bstrVal;
+      VariantClear(&vProperty);
     }
+    pclassObject->lpVtbl->Release(pclassObject);
   }
 
-  if (FAILED(*hres) || ures == 0) {
-    return res;
-  }
-
-  VARIANT vProperty;
-  *hres = pclassObject->lpVtbl->Get(pclassObject, field, 0, &vProperty, 0, 0);
-
-  if (!FAILED(*hres)) {
-    res = vProperty.bstrVal;
-    VariantClear(&vProperty);
-  }
-
-  pclassObject->lpVtbl->Release(pclassObject);
-  return res;
+  return hres;
 }
 
-wchar_t* getOS(HRESULT* hres) {
-  wchar_t* res = L"";
+HRESULT getOS(wchar_t* os) {
+  HRESULT hres = init();
+  if (FAILED(hres)) {
+    CoUninitialize();
+    return hres;
+  }
 
-  if (is_valid_hresult(hres)) {
-    init(hres);
-    if (FAILED(*hres)) {
-      CoUninitialize();
-      return res;
-    }
+  IWbemLocator* plocator = NULL;
+  hres = createInstance(plocator);
+  if (FAILED(hres)) {
+    CoUninitialize();
+    return hres;
+  }
 
-    IWbemLocator *plocator = createInstance(hres);
-    if (FAILED(*hres)) {
-      CoUninitialize();
-      return res;
-    }
+  IWbemServices* pservices = NULL;
+  hres = connectServer(plocator, pservices);
+  if (FAILED(hres)) {
+    pservices->lpVtbl->Release(pservices);
+    plocator->lpVtbl->Release(plocator);
+    CoUninitialize();
+    return hres;
+  }
 
-    IWbemServices *pservices = connectServer(hres, plocator);
-    if (FAILED(*hres)) {
-      pservices->lpVtbl->Release(pservices);
-      plocator->lpVtbl->Release(plocator);
-      CoUninitialize();
-      return res;
-    }
-
-    IEnumWbemClassObject* penumerator = query(hres,
-                                              pservices,
-                                              SysAllocString(L"SELECT * FROM Win32_OperatingSystem"));
-    if (FAILED(*hres)) {
-      penumerator->lpVtbl->Release(penumerator);
-      pservices->lpVtbl->Release(pservices);
-      plocator->lpVtbl->Release(plocator);
-      CoUninitialize();
-      return res;
-    }
-
-    res = getStringField(hres, penumerator, SysAllocString(L"Caption"));
-    if (FAILED(*hres)) {
-      res = L"";
-    }
-
+  IEnumWbemClassObject* penumerator = NULL;
+  hres = query(pservices, SysAllocString(L"SELECT * FROM Win32_OperatingSystem"), penumerator);
+  if (FAILED(hres)) {
     penumerator->lpVtbl->Release(penumerator);
     pservices->lpVtbl->Release(pservices);
     plocator->lpVtbl->Release(plocator);
     CoUninitialize();
+    return hres;
   }
 
-  return res;
+  hres = getStringField(penumerator, SysAllocString(L"Caption"), os);
+
+  penumerator->lpVtbl->Release(penumerator);
+  pservices->lpVtbl->Release(pservices);
+  plocator->lpVtbl->Release(plocator);
+  CoUninitialize();
+
+  return hres;
 }
