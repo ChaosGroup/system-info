9568b81c8afcddc631bfd03604a7be7c560d6169
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-02-06 12:15:33 +0200

    Reuse the version variable instead of calling atoi twice

--- csrc/uname.c
+++ csrc/uname.c
@@ -21,7 +21,7 @@ wchar_t* getOS() {
     format = L"Mac OS X 10.%d";
   }
 
-  if (swprintf(os, OS_VERSION_MAX_SIZE - 1, format, atoi(system_info.release) - 4) == -1) {
+  if (swprintf(os, OS_VERSION_MAX_SIZE - 1, format, version - 4) == -1) {
 #else
   if (swprintf(os, OS_VERSION_MAX_SIZE - 1, L"%s %s", system_info.sysname, system_info.release) == -1) {
 #endif
